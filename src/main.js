1582533f25ce894bd653fbcebc5fc602d5c0d69e
Roni Choudhury <roni.choudhury@kitware.com>
2018-03-28 17:40:41 -0400

    Add a function using an external library

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { awesomeAdd, later } from '../dist/codesplit';
+import { add, later, getText } from '../dist/codesplit';
 
-document.write(`<h1>10 + 3 is ${awesomeAdd(10, 3)}</h1>`);
+document.write(`<h1>10 + 3 is ${add(10, 3)}</h1>`);
 
 console.log('calling later()');
 later(10);
+
+console.log('calling getText()');
+(async function () {
+  let txt = await getText('README.md');
+  console.log('txt', txt);
+}());
