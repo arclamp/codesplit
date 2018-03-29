97fdcc7d00b92f00ec389ae4d5d702165222dce7
Roni Choudhury <roni.choudhury@kitware.com>
2018-03-29 17:49:38 -0400

    Wrap entire main app in an async IIFE

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import { add, later, getText } from '../dist/codesplit';
 
-document.write(`<h1>10 + 3 is ${add(10, 3)}</h1>`);
-
-console.log('calling later()');
-later(10);
-
-console.log('calling getText()');
 (async function () {
+  document.write(`<h1>10 + 3 is ${add(10, 3)}</h1>`);
+
+  console.log('calling later()');
+  later(10);
+
+  console.log('calling getText()');
+
   let txt = await getText('README.md');
   console.log('txt', txt);
 }());
